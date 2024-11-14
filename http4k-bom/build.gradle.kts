5d6eb12703085f69307716c53ec895ad99101619
David Denton <denton.david@gmail.com>
2024-11-14 14:50:00 +0000

    Add gradle getting all artifacts

--- http4k-bom/build.gradle.kts
+++ http4k-bom/build.gradle.kts
@@ -1,5 +1,27 @@
 description = "Http4k Bill Of Materials (BOM)"
 
+plugins {
+    java
+}
+
+val http4kLocalHavenRepo = file("${System.getProperty("user.home")}/.m2/repository/org/http4k")
+
+val bomModules = when {
+    http4kLocalHavenRepo.exists() && http4kLocalHavenRepo.isDirectory ->
+        http4kLocalHavenRepo.listFiles()
+            ?.filter { it.isDirectory }
+            ?.map { it.name }?.toList()
+            ?: emptyList()
+
+    else -> emptyList()
+}
+
+
 dependencies {
+    bomModules
+        .sorted()
+        .forEach {
+            println(it)
+        }
 }
 
